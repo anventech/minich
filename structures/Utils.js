9a2e53813ddbfeebb7abcf267d9a01fee93da4fc
anventech <dev.anventec@gmail.com>
2021-09-02 21:34:46 -0400

    Función de hacer URLs en base a la documentación.

--- structures/Utils.js
+++ structures/Utils.js
@@ -1,5 +1,10 @@
 module.exports = {
   betweenCor: (string) => {
     return `[${string}]`;
+  },
+  makeDocs: (page, id) => {
+    const docs = "https://minich-docs.github.io";
+
+    return `${docs}/${page}?id=${id}`;
   }
 }
